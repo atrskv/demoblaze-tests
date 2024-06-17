ad9cf53271dc32a29b183c5592cf49e72948ab46
lrayne <legaterayne@gmail.com>
2024-06-18 00:00:25 +0300

    Add wait_until_alert_is_present()

--- demoblaze_tests/utils.py
+++ demoblaze_tests/utils.py
@@ -1,7 +1,15 @@
 import random
 import string
+from selene import browser
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def random_string(length):
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for _ in range(length))
+
+
+def wait_until_alert_is_present():
+    wait = WebDriverWait(browser.driver, 4)
+    wait.until(expected_conditions.alert_is_present())
