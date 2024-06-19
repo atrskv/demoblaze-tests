90af652e3b0cea4a3fe3f16824da027ea1bf22cc
lrayne <legaterayne@gmail.com>
2024-06-19 13:37:56 +0300

    Refactor tests/test_products.py

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,12 +1,13 @@
 from selene import browser, have
 from selene.support.shared.jquery_style import s
 from demoblaze_tests.utils import wait_until_alert_is_present
-from demoblaze_tests.data.products import monitor, phone
+from demoblaze_tests.data import products
 
 
 def test_add_a_product_to_the_cart():
 
     # GIVEN
+    phone = products.phone
     browser.open('https://www.demoblaze.com')
 
     # WHEN
@@ -20,17 +21,20 @@ def test_add_a_product_to_the_cart():
     wait_until_alert_is_present()
     browser.driver.switch_to.alert.accept()
 
-    s('#navbarExample').s('#cartur').click()
+    s('.navbar-nav').s('#cartur').click()
 
     # THEN
-    s('#tbodyid').ss('tr.success').first.ss('td').second.should(
+    s('#tbodyid').ss('tr').first.ss('td').second.should(
         have.exact_text(phone.name)
     )
+
+    s('#totalp').should(have.exact_text(phone.price))
 
 
 def test_filter_the_products_by_monitors_category():
 
     # GIVEN
+    monitor = products.monitor
     browser.open('https://www.demoblaze.com')
 
     # WHEN
@@ -39,7 +43,6 @@ def test_filter_the_products_by_monitors_category():
     ).click()
 
     # THEN
-    s('#tbodyid').ss('.card-title').should(have.size(2))
-    s('#tbodyid').ss('.card-title').second.should(
-        have.exact_text(monitor.name)
-    )
+    cards = s('#tbodyid').ss('.card-title')
+    cards.should(have.size(2))
+    cards.second.should(have.exact_text(monitor.name))
