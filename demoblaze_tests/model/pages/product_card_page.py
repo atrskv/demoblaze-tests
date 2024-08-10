36f4ceada8d2fc8731bfbdc4c3f0b7e3c751ad04
lrayne <legaterayne@gmail.com>
2024-08-10 21:16:00 +0300

    Fix

--- demoblaze_tests/model/pages/product_card_page.py
+++ demoblaze_tests/model/pages/product_card_page.py
@@ -3,11 +3,13 @@ from selene.support.shared.jquery_style import s
 from selene import browser
 from demoblaze_tests.data.products import Product
 from demoblaze_tests.model.components.menu import Menu
+from demoblaze_tests.model.components.modal_content import Alert
 
 
 class ProductCardPage:
     def __init__(self):
         self.menu = Menu()
+        self.alert = Alert()
 
     @allure.step('Открыть карточку товара \'{product}\'')
     def open(self, product: Product):
@@ -16,3 +18,4 @@ class ProductCardPage:
     @allure.step('В карточке товара нажать на кнопку \'Add to cart\'')
     def add_product_to_cart(self):
         s('.product-content').s('.btn-success').click()
+        self.alert.accept()
